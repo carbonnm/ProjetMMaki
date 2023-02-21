2586718ee9edd5a5304e8a9d809f8b888aa313fb
IGoldenMemories <GallifreyArcadia@gmail.com>
2023-02-21 14:24:31 +0100

    homescreen revamped

--- MainMenu.gd
+++ MainMenu.gd
@@ -4,3 +4,19 @@ export var mainScene : PackedScene
 
 func _on_BoutonCreer_button_up():
 	get_tree().change_scene("res://Main.tscn")
+
+#AESTHETIC (launches little arrow animation on mouse entered)
+func _on_BoutonCreer_mouse_entered():
+	get_child(2).get_child(1).playing = true
+
+#AESTHETIC (stops little arrow animation on mouse entered)
+func _on_BoutonCreer_mouse_exited():
+	get_child(2).get_child(1).playing = false
+
+#AESTHETIC (launches little arrow animation on mouse entered)
+func _on_BoutonOptions_mouse_entered():
+	get_child(3).get_child(1).playing = true
+
+#AESTHETIC (stops little arrow animation on mouse entered)
+func _on_BoutonOptions_mouse_exited():
+	get_child(3).get_child(1).playing = false
