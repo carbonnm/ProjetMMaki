74f0ebe58ac0649a9f35b4b5daa040fe739e230a
carbonnm <55496761+carbonnm@users.noreply.github.com>
2023-04-02 13:15:10 +0200

    Test OpenCV sur une nouvelle scène + installation d'une librairie alternative à easyocr

---
+++ Scene/TestOpenCVPython.py
@@ -0,0 +1,20 @@
+from godot import exposed, export
+from godot import *
+import cv2
+import matplotlib
+import easyocr
+
+
+@exposed
+class TestOpenCVPython(Node2D):
+
+	# member variables here, example:
+	a = export(int)
+	b = export(str, default='foo')
+
+	def _ready(self):
+		"""
+		Called every time the node is added to the scene.
+		Initialization here.
+		"""
+		pass
