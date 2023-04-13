8a8813a3b5498feb809ee51af6b5ce87367f303a
carbonnm <55496761+carbonnm@users.noreply.github.com>
2023-04-13 10:30:52 +0200

    Ajout de la pop up avec les 4 images (pour le moment cookies sont harcodés)

---
+++ Scripts/ChoixImage.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+
+func _on_OKButton_pressed():
+	 visible = false
+
+
+func _on_Image1_pressed():
+	pass # Replace with function body.
+
+
+func _on_Image2_pressed():
+	pass # Replace with function body.
+
+
+func _on_Image3_pressed():
+	pass # Replace with function body.
+
+
+func _on_Image4_pressed():
+	pass # Replace with function body.
--- Scripts/WordRecognizedPopUp.gd
+++ Scripts/WordRecognizedPopUp.gd
@@ -7,3 +7,6 @@ func _on_exitButton_pressed():
 
 func _on_okButton_pressed():
 	visible = false
+	var size_pop_up : Vector2 = get_node("../ChoixImage/ColorRect").get_global_rect().size
+	get_node("../ChoixImage").rect_position = get_node("../Camera").get_camera_position() - size_pop_up - Vector2(120, -200)
+	get_node("../ChoixImage").visible = true
