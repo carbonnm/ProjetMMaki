3112618696b9b0d2734b5cf3afbcc9333a19881e
carbonnm <55496761+carbonnm@users.noreply.github.com>
2023-04-13 10:51:37 +0200

    Affichage de l'image sélectionnée par la pop up sur l'écran

--- Scripts/ChoixImage.gd
+++ Scripts/ChoixImage.gd
@@ -1,22 +1,32 @@
 extends Control
 
+onready var image1 = $Image1
+onready var image2 = $Image2
+onready var image3 = $Image3
+onready var image4 = $Image4
+
+var chosen_button_icon
 
 
 func _on_OKButton_pressed():
-	 visible = false
+	var drawing = Sprite.new()
+	drawing.texture = chosen_button_icon
+	drawing.position = Vector2(500, 300)
+	get_node("../Elements").add_child(drawing)
+	visible = false
 
 
 func _on_Image1_pressed():
-	pass # Replace with function body.
+	chosen_button_icon = image1.icon
 
 
 func _on_Image2_pressed():
-	pass # Replace with function body.
+	chosen_button_icon = image2.icon
 
 
 func _on_Image3_pressed():
-	pass # Replace with function body.
+	chosen_button_icon = image3.icon
 
 
 func _on_Image4_pressed():
-	pass # Replace with function body.
+	chosen_button_icon = image4.icon
