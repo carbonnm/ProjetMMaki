ad3e80b3e978f82f9f89d531b1bba4d9c0ad07d2
abarbieux <arthur.barbieux@student.unamur.be>
2023-04-15 13:39:07 +0200

    group debug

--- Classes/GroupManager.gd
+++ Classes/GroupManager.gd
@@ -7,21 +7,15 @@ var groups: Array = []
 func input(event: InputEvent) -> IState:
 	var elements: Array = canvas.Utils.map(canvas.selected_lines, canvas.Mimic, "get_first",[])
 	elements = elements.slice(0,elements.size()-2)
-	var positions = canvas.Utils.map(elements, canvas.Mimic, "area2D_position", [])
-	var closure = canvas.Utils.get_positions_closure(positions)
-	var center = canvas.Utils.get_positions_mean(closure)
-	var transform := Transform2D()
-	transform.origin = center
 	
 	var group := Area2D.new()
-	group.name = "group" + str(groups.size())
-	group.global_transform = transform
+	group.name = "group" + str(groups.size()-1)
 	
 	# retrieve elements from selection
 	var childs: Array = []
 	for element in elements:
 		childs += element.get_children()
-
+		element.get_parent().remove_child(element)
 		
 		
 	# save ungroup elements
@@ -30,18 +24,10 @@ func input(event: InputEvent) -> IState:
 	# add childs to group
 	for child in childs:
 		var child_copy = child.duplicate(true)
-		child_copy.global_transform= child.global_transform
-		child_copy.position -= center
 		group.add_child(child_copy)
 	
 	#add group to scene
 	canvas._elements.add_child(group)
 	
-	# remove child from element
-	for element in elements:
-		element.get_parent().remove_child(element)
-	
-	#add the group to selected_lines
-	canvas.selected_lines = [group]
 	return switch_to_previous_state()
 
