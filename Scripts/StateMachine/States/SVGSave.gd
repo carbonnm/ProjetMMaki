2d93ddb60fbd342235178d56fd15d54d191df058
Simon POLET <32013966+Fogbose@users.noreply.github.com>
2023-04-21 13:25:29 +0200

    Correction de l'état SaveSVG

--- Scripts/StateMachine/States/SVGSave.gd
+++ Scripts/StateMachine/States/SVGSave.gd
@@ -3,18 +3,22 @@ extends AState
 var SVGCompiler := preload("res://SavePlugin/SVGCompiler.gd")
 
 func enter():
-	canvas.get_node("SavePopUpConfirmation").rect_position = Vector2(400, 200)
+	var viewport_center = canvas._camera.global_position
+
+	canvas.get_node("SavePopUpConfirmation").rect_position = viewport_center
 	canvas.get_node("SavePopUpConfirmation").visible = true
 
-func input(_event: InputEvent) -> IState:
-	if canvas.get_node("SavePopUpConfirmation").validation == null:
-		return null
+func physics_process(delta) -> IState:
+	var spuc: Node = canvas.get_node("SavePopUpConfirmation")
 	
-	if canvas.get_node("SavePopUpConfirmation").validation:
-
+	if spuc.validation:
 		var save_node = canvas.Utils.get_match_string_node("Elements", canvas)
 		var background_node = canvas.Utils.get_match_string_node("BackgroundColor", canvas)
 		var svg_compiler = SVGCompiler.new([save_node, background_node])
 		svg_compiler.export_to_svg()
-
-	return switch_to_previous_state()
+	
+	if spuc.visible:
+		return null
+	
+	else:
+		return switch_to_previous_state()
