2c41bdfbc30045f4804cb3935e9c39cae793ba19
IGoldenMemories <GallifreyArcadia@gmail.com>
2023-04-25 12:21:22 +0200

    nothing to see here :eyes:

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,6 +1,32 @@
 extends Node2D
 
 export var mainScene : PackedScene
+var donetimer = false
+func _ready():
+	var timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(timer) #to process
+	timer.start() #to start
+
+
+func _on_timer_timeout():
+	
+	var noderef = get_node("Nothingtoseehere")
+	var speakref = get_node("Nothingtoseehere/Notaneasteregg/Duckspeak")
+	noderef.visible = true
+	
+	if not donetimer:
+		get_node("Nothingtoseehere/Notaneasteregg").playing = true
+		speakref.percent_visible = 0.0
+		speakref.visible_characters = 0
+		yield(get_tree().create_timer(3), "timeout")
+		var x = 1
+		while x < speakref.bbcode_text.length():
+			
+			speakref.set("visible_characters",x)
+			yield(get_tree().create_timer(0.05), "timeout")
+			x+=9
+		donetimer = true
 
 #Going to the next scene (the new canvas options menu)
 func _on_BoutonCreer_button_up():
@@ -30,5 +56,6 @@ func _on_BoutonOptions_mouse_exited():
 
 
 func _on_BoutonOptions_pressed():
-	get_node("Namenotentered").visible = true
+	#("")
+	pass
 
