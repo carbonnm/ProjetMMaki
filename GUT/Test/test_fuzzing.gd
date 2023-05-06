458da18014c2514c58ca435243b83fb6c9fa7da2
Simon POLET <32013966+Fogbose@users.noreply.github.com>
2023-05-06 14:27:22 +0200

    Ajout d'un oracle

--- GUT/Test/test_fuzzing.gd
+++ GUT/Test/test_fuzzing.gd
@@ -67,17 +67,38 @@ func test_fuzzing():
 	for i in range(1000):
 		# Arrange
 		var event = create_random_event()
-		event_sequence.append(event)
 		# Act
 		viewport.input(event)
+		
+		var log_file: File = File.new()
+		var opened: int = log_file.open("res://GUT/Log/event_sequence.txt", File.WRITE)
+		if opened != 0:
+			print("Error on file opening.")
+		else:
+			var string_to_store = ""
+			var k: int = 1
+			for element in event_sequence:
+					
+				string_to_store += str(element)
+				
+				if k % 10 == 0:
+					string_to_store += "\n -> "
+				else:
+					string_to_store += " -> "
+					
+				k += 1
+			
+			string_to_store.rstrip(4)
+					
+			log_file.store_string(string_to_store)
+			log_file.close()
+				
 		# Assert
 		var crashed = OS.has_environment("godot_crash")
 		assert_false(crashed, "Le programme a planté avec l'événement aléatoire numéro " + str(i))
 
 func create_random_event():
-	var kind_of_event_total_weight: int = 0
-	for element in kind_of_event:
-		kind_of_event_total_weight += int(element)
+	var kind_of_event_total_weight: int = kind_of_event.One_Shot_Event + kind_of_event.Process_Event + kind_of_event.Jail_Event + kind_of_event.Random_Key_Event
 		
 	var random_kind_of_event: int = rng.randi_range(1,kind_of_event_total_weight)
 	
@@ -111,48 +132,69 @@ func get_random_one_shot_event():
 	
 	match random_event:
 		one_shot_event.Key_Space:
+			event_sequence.append(OS.get_scancode_string(KEY_SPACE))
 			return InputFactory.key_down(KEY_SPACE)
 		one_shot_event.Key_D:
+			event_sequence.append(OS.get_scancode_string(KEY_D))
 			return InputFactory.key_down(KEY_D)
 		one_shot_event.Key_F:
+			event_sequence.append(OS.get_scancode_string(KEY_F))
 			return InputFactory.key_down(KEY_F)
 		one_shot_event.Key_G:
+			event_sequence.append(OS.get_scancode_string(KEY_G))
 			return InputFactory.key_down(KEY_G)
 		one_shot_event.Key_R:
+			event_sequence.append(OS.get_scancode_string(KEY_R))
 			return InputFactory.key_down(KEY_R)
 		one_shot_event.Key_S:
+			event_sequence.append(OS.get_scancode_string(KEY_S))
 			return InputFactory.key_down(KEY_S)
 		one_shot_event.Key_Z:
+			event_sequence.append(OS.get_scancode_string(KEY_Z))
 			return InputFactory.key_down(KEY_Z)
 		one_shot_event.Key_Bksp:
+			event_sequence.append(OS.get_scancode_string(KEY_BACKSPACE))
 			return InputFactory.key_down(KEY_BACKSPACE)
 		one_shot_event.Key_Del:
+			event_sequence.append(OS.get_scancode_string(KEY_DELETE))
 			return InputFactory.key_down(KEY_DELETE)
 		one_shot_event.Key_Left:
+			event_sequence.append(OS.get_scancode_string(KEY_LEFT))
 			return InputFactory.key_down(KEY_LEFT)
 		one_shot_event.Key_Up:
+			event_sequence.append(OS.get_scancode_string(KEY_UP))
 			return InputFactory.key_down(KEY_UP)
 		one_shot_event.Key_Right:
+			event_sequence.append(OS.get_scancode_string(KEY_RIGHT))
 			return InputFactory.key_down(KEY_RIGHT)
 		one_shot_event.Key_Down:
+			event_sequence.append(OS.get_scancode_string(KEY_DOWN))
 			return InputFactory.key_down(KEY_DOWN)
-		
+			
 		one_shot_event.Macro_Ctrl_Plus:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_PLUS))
 			return InputFactory.action_down("Zoom")
 		one_shot_event.Macro_Ctrl_Minus:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_MINUS))
 			return InputFactory.action_down("Unzoom")
 		one_shot_event.Macro_Ctrl_C:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_C))
 			return InputFactory.action_down("Copy")
 		one_shot_event.Macro_Ctrl_V:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_V))
 			return InputFactory.action_down("Paste")
 		one_shot_event.Macro_Ctrl_Y:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_Y))
 			return InputFactory.action_down("Redo")
 		one_shot_event.Macro_Ctrl_Z:
+			event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_Z))
 			return InputFactory.action_down("Undo")
 		
 		one_shot_event.Mouse_Roll_Up:
+			event_sequence.append("Wheel Up")
 			return InputFactory.new_mouse_button_event(mouse_position,mouse_position,true,BUTTON_WHEEL_UP)
 		one_shot_event.Mouse_Roll_Down:
+			event_sequence.append("Wheel Down")
 			return InputFactory.new_mouse_button_event(mouse_position,mouse_position,true,BUTTON_WHEEL_DOWN)
 
 func get_random_process_event():
@@ -161,19 +203,26 @@ func get_random_process_event():
 	
 	match random_event:
 		process_event.Mouse_Button_Left:
+			event_sequence.append("Button Left" + " " + str(mouse_position))
 			return InputFactory.new_mouse_button_event(mouse_position,mouse_position,true,BUTTON_LEFT)
 		process_event.Mouse_Button_Middle:
+			event_sequence.append("Button Middle" + " " + str(mouse_position))
 			return InputFactory.new_mouse_button_event(mouse_position,mouse_position,true,BUTTON_MIDDLE)
 
 func get_jail_event():
-	if event_sequence[-1] is InputEventKey and event_sequence[-1].scancode == KEY_S:
+	var random: int = rng.randi_range(0,1)
+	
+	if random == 1:
+		event_sequence.append("Enter")
 		return InputFactory.key_down(KEY_ENTER)
 	
 	else:
+		event_sequence.append(OS.get_scancode_string(KEY_MASK_CTRL) + OS.get_scancode_string(KEY_S))
 		return InputFactory.action_down("Save")
 
 func get_random_key_event():
 	var random_key: int = rng.randi_range(0,255)
+	event_sequence.append(OS.get_scancode_string(random_key))
 	return InputFactory.key_down(random_key)
 
 func jittler_mouse_position():
