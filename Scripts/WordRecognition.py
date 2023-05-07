a06146b5160ee7c6c522ad44f9469e8fe6775462
ManonGlloy <manon.galloy@student.unamur.be>
2023-05-07 21:20:18 +0200

    Delete useless import

--- Scripts/WordRecognition.py
+++ Scripts/WordRecognition.py
@@ -1,6 +1,4 @@
 from PIL import Image
-import cv2
-import matplotlib
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -11,5 +9,4 @@ im = Image.open(pathImage)
 
 jpg_im = im.convert("RGB")
 
-#image = cv2.imread(r'testPythonOpenCV.png')
 print(pytesseract.image_to_string(jpg_im))
